a64deec7545f2b88dd98186fd90e99f70ec024ea
Per Kristian Svanberg <45123507+lutefisk-development@users.noreply.github.com>
2021-09-06 21:43:25 +0200

    Create app.vue
    WIP - placeholder

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="heading">
+        <h1 class="heading__header">Hello world!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
