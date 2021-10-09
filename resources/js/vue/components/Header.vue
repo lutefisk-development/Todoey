ab47dc53d4bc1b277bcbc5b987f1c9d45cfb6492
Per Kristian Svanberg <45123507+lutefisk-development@users.noreply.github.com>
2021-10-09 15:10:54 +0200

    Created header component

---
+++ resources/js/vue/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="header container mx-auto h-40 bg-gray-300 rounded-sm flex justify-center items-center mt-12">
+        <h1 class="header__heading text-white text-3xl font-bold italic font-display">ToDoey</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
